3f4c97f743edab07067253c4101205f498f909b5
Sávio Sant'anna <saviosantannadesa@gmail.com>
2023-10-10 21:32:59 -0300

    Novo ajuste

--- api/header.php
+++ api/header.php
@@ -1,8 +1,3 @@
-<?php
-if (!isset($_SESSION)) {
-    session_start();
-} ?>
-
 <!DOCTYPE html>
 <html lang="en" class="min-vh-100">
 
@@ -11,10 +6,10 @@ if (!isset($_SESSION)) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
 
-
+    
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous" defer></script>
-
+    
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 
     <link rel="shortcut icon" href="img/favicon-saviosantanna.png" type="image/x-icon">
@@ -30,14 +25,16 @@ if (!isset($_SESSION)) {
         </div>
 
         <!-- Mostrar botão de sair se o usuário estiver logado -->
-
-        <?php
-        if (isset($_SESSION['login'])) {
+        <?php 
+        if(!isset($_SESSION)){
+            session_start();
+        }
+        if(isset($_SESSION['login'])){
         ?>
-            <div class="me-4 m-2">
-                <a href="checkLogout.php" class="text-decoration-none text-white"><button class="btn btn-danger" name="btn-sair">Sair</button></a>
-            </div>
-        <?php } ?>
+        <div class="me-4 m-2">
+            <a href="checkLogout.php" class="text-decoration-none text-white"><button  class="btn btn-danger" name="btn-sair">Sair</button></a>
+        </div>
+        <?php }?>
 
     </nav>
     <div class="container border-bottom border-dark border-2 w-75"></div>
